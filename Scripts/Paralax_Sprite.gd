ff1b07971f044b946e98abbe256582636aaa986f
schmertenk <49512941+schmertenk@users.noreply.github.com>
2021-01-14 14:06:24 +0100

    added level 11

--- Scripts/Paralax_Sprite.gd
+++ Scripts/Paralax_Sprite.gd
@@ -3,7 +3,9 @@ extends Sprite
 export (Vector2) var motion_scale = Vector2(1,1)
 export (NodePath) var camera_path
 
-onready var camera = get_node(camera_path)
+onready var camera : Camera2D = get_node(camera_path)
 onready var initial_position = position
 func _process(_delta):
 	position = camera.get_camera_screen_center() * motion_scale + initial_position
+
+	
